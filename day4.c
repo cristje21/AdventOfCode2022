13e7f72110bb3dffc3ec93d279311db4c882862e
Cris <cristje@Cris.His.Laptop>
2022-12-04 12:13:53 +0100

    done

--- day4.c
+++ day4.c
@@ -14,15 +14,29 @@ int	check_for_full_overlap(int *pair)
 	return (0);
 }
 
-int	part_one_func(FILE *fp)
+int	check_for_some_overlap(int *pair)
+{
+	if (pair[0] >= pair[2] && pair[0] <= pair[3])
+		return (1);
+	else if (pair[1] >= pair[2] && pair[1] <= pair[3])
+		return (1);
+	else if (pair[2] >= pair[0] && pair[2] <= pair[1])
+		return (1);
+	else if (pair[3] >= pair[0] && pair[3] <= pair[1])
+		return (1);
+	return (0);
+}
+
+int	part_both_func(FILE *fp)
 {
 	char	line[15];
 	int		pair[4];
 	int		i, j;
-	int		result;
+	int		result[2];
 
 	fgets(line, 15, fp);
-	result = 0;
+	result[0] = 0;
+	result[1] = 0;
 	while (line[0] != '.')
 	{
 		i = 0;
@@ -34,10 +48,12 @@ int	part_one_func(FILE *fp)
 				j++;
 			j++;
 		}
-		result += check_for_full_overlap(pair);
+		result[0] += check_for_full_overlap(pair);
+		result[1] += check_for_some_overlap(pair);
 		fgets(line, 15, fp);
 	}
-	return (result);
+	printf("part 1: %d\npart 2: %d\n", result[0], result[1]);
+	return (0);
 }
 
 int	main(void)
@@ -52,8 +68,7 @@ int	main(void)
 		printf("something went wrong opening the file.\n");
 		return (1);
 	}
-	part_one = part_one_func(fp);
-	printf("%d\n", part_one);
+	part_both_func(fp);
 	fclose(fp);
 	return (0);
 }
